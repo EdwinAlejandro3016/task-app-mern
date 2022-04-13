87fb675d64f87cc5c3fd8f4c7aceea35ee714bb9
acye <edwinalejandro3016@gmail.com>
2022-04-13 10:21:38 -0500

    first

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+
+
+function App() {
+  return (
+    <div>
+      <header>
+          <a href="/">amazona</a>
+      </header>
+      <main>
+        List Products
+      </main>
+      <footer>
+        footer
+      </footer>
+    </div>
+  );
+}
+
+export default App;
